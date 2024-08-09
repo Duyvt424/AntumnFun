da54645c106c93d0a3afff832e011ffd51d5e0fe
mongbien <duyvtph24890@fpt.edu.vn>
2024-08-09 16:11:22 +0700

    Add project files.

---
+++ TestCoding/Two sum.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCoding
+{
+    public class Two_sum
+    {
+        static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return new int[] {};
+        }
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            while (true)
+            {
+                Console.Write("Mời bạn nhập chiều dài mảng: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                int[] numArr = new int[n];
+
+                Console.WriteLine("Mời bạn nhập các phần tử của mảng: ");
+                for (int i = 0; i < n; i++)
+                {
+                    numArr[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                Console.Write("Mời bạn nhập target: ");
+                int target = Convert.ToInt32(Console.ReadLine());
+
+                int[] result = TwoSum(numArr, target);
+                Console.Write("Pivot integer +)): ");
+                foreach (var item in result)
+                {
+                    Console.Write(item + " ");
+                }
+                if (n == 424)
+                {
+                    Environment.Exit(n);
+                }
+            }
+        }
+    }
+}
